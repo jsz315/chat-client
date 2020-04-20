c4fad0c4236fc00f8867125e34eb83c319c7140b
jsz315 <jsz_thing@126.com>
2020-04-20 17:44:40 +0800

    修改多人联机

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,7 @@
 
 <script>
 import Room from './components/room/index.vue'
+import './core/client'
 
 export default {
   name: 'app',
---
+++ src/core/client.js
@@ -0,0 +1,54 @@
+// import listener from './listener'
+// import Message from './message'
+
+var socket;
+
+function init(url){
+    socket = io(url);
+}
+
+function send(type, data){
+    socket.emit(type, data);
+}
+
+function on(type, callback){
+    socket.on(type, callback);
+}
+
+/*
+listener.on('init', (url)=>{
+    socket = io(url);
+
+    socket.on('connect', ()=>{
+        console.log('socket connect');
+        listener.emit('connect');
+    });
+    
+    socket.on('disconnect', ()=>{
+        console.log('socket disconnect');
+
+        listener.emit('disconnect');
+    });
+
+    socket.on(Message.TYPE_MESSAGE, (data)=>{
+        console.log('socket ' + Message.TYPE_MESSAGE);
+        console.log(data);
+
+        var obj = JSON.parse(data);
+        listener.emit(Message.TYPE_MESSAGE, obj.data);
+    });
+})
+
+listener.on('send', obj=>{
+    console.log('send ' + obj.type);
+    console.log(obj);
+    socket.emit(obj.type, obj);
+})
+*/
+
+export default {
+    socket,
+    init,
+    send,
+    on
+}
